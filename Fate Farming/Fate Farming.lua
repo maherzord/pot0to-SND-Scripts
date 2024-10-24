45343db37a16127b571b3d235b93c33ee1a41feb
maherzord <77556001+maherzord@users.noreply.github.com>
2024-10-24 16:41:00 -0300

    adding configuration profiles

--- Fate Farming/Fate Farming.lua
+++ Fate Farming/Fate Farming.lua
@@ -80,79 +80,41 @@ This Plugins are Optional and not needed unless you have it enabled in the setti
 
 player_first_string = string.sub(GetCharacterName(),1,1)
 
+function executeScriptFromURL(url)
+    -- Command to fetch the Lua script using curl
+    local command = string.format('curl -s "%s"', url)
+
+    -- Open a pipe to read the output of the command
+    local pipe = io.popen(command)
+
+    -- Read the output of the command (the Lua script)
+    local scriptContent = pipe:read("*a")
+    pipe:close()
+
+    -- Load and execute the Lua script
+    local loadedFunction, errorMessage = load(scriptContent)
+    if loadedFunction then
+        loadedFunction()  -- Execute the loaded function
+        yield("/echo Configuration Profile loaded and executed successfully.")
+    else
+        yield("/echo Error loading configuration profile:", errorMessage)
+    end
+end
 --Pre Fate Settings
 if player_first_string == "S" then
-    yield("/echo Starting "..player_first_string.." configuration.")
-    Food = ""                           --Leave "" Blank if you don't want to use any food. If its HQ include <hq> next to the name "Baked Eggplant <hq>"
-    Potion = ""                         --Leave "" Blank if you don't want to use any potions.
-    ShouldSummonChocobo = true          --Summon chocobo?
-        ShouldAutoBuyGysahlGreens = true    --Automatically buys a 99 stack of Gysahl Greens from the Limsa gil vendor if you're out
-    MountToUse = "mount roulette"       --The mount you'd like to use when flying between fates
-
---Fate Combat Settings
-    CompletionToIgnoreFate = 80         --If the fate has more than this much progress already, skip it
-    MinTimeLeftToIgnoreFate = 2*60      --If the fate has less than this many seconds left on the timer, skip it
-    CompletionToJoinBossFate = 5        --If the boss fate has less than this much progress, skip it (used to avoid soloing bosses)
-        CompletionToJoinSpecialBossFates = 20   --For the Special Fates like the Serpentlord Seethes or Mascot Murder
-        ClassForBossFates = ""              --If you want to use a different class for boss fates, set this to the 3 letter abbreviation
-                                        --for the class. Ex: "PLD"
-    JoinCollectionsFates = true         --Set to false if you never want to do collections fates
-    RSRAoeType = "Full"               --Options: Cleave/Full/Off
-    RSRAutoType = "HighHP"               --Options: LowHP/HighHP/Big/Small/HighMaxHP/LowMaxHP/Nearest/Farthest.
-                                    
-    UseBM = true                        --if you want to use the BossMod dodge/follow mode
-        BMorBMR = "BMR"
-        MeleeDist = 2.5                     --distance for BMRAI melee. Melee attacks (auto attacks) max distance is 2.59y, 2.60 is "target out of range"
-        RangedDist = 10                     --distance for BMRAI ranged. Ranged attacks and spells max distance to be usable is 25.49y, 25.5 is "target out of range"=
-
---Post Fate Settings
-    EnableChangeInstance = true                     --should it Change Instance when there is no Fate (only works on DT fates)
-        WaitIfBonusBuff = true                          --Don't change instances if you have the Twist of Fate bonus buff
-    ShouldExchangeBicolorVouchers = true            --Should it exchange Bicolor Gemstone Vouchers?
-        VoucherType = "Bicolor Gemstone Voucher"        -- Old Sharlayan for "Bicolor Gemstone Voucher" and Solution Nine for "Turali Bicolor Gemstone Voucher"
-    SelfRepair = true                              --if false, will go to Limsa mender
-        RepairAmount = 99                               --the amount it needs to drop before Repairing (set it to 0 if you don't want it to repair)
-        ShouldAutoBuyDarkMatter = true                  --Automatically buys a 99 stack of Grade 8 Dark Matter from the Limsa gil vendor if you're out
-    ShouldExtractMateria = true                           --should it Extract Materia
-    Retainers = false                                --should it do Retainers
-    ShouldGrandCompanyTurnIn = false                --should it to Turn ins at the GC (requires Deliveroo)
-        InventorySlotsLeft = 5                          --how much inventory space before turning in
+    yield("/echo Starting Maher configuration profile.")
+-- URL of the Lua script to fetch and execute
+    local scriptURL = "https://raw.githubusercontent.com/maherzord/pot0to-SND-Scripts/refs/heads/main/Fate%20Farming/Configuration%20Profile/Maher.lua"
+--local scriptURL = "https://example.com/script.lua"
+-- Execute the Lua script fetched from the URL
+    executeScriptFromURL(scriptURL)        
 elseif player_first_string == "H" then
-    yield("/echo Starting "..player_first_string.." configuration.")        
-    Food = ""                           --Leave "" Blank if you don't want to use any food. If its HQ include <hq> next to the name "Baked Eggplant <hq>"
-    Potion = ""                         --Leave "" Blank if you don't want to use any potions.
-    ShouldSummonChocobo = true          --Summon chocobo?
-        ShouldAutoBuyGysahlGreens = true    --Automatically buys a 99 stack of Gysahl Greens from the Limsa gil vendor if you're out
-    MountToUse = "mount roulette"       --The mount you'd like to use when flying between fates
-
---Fate Combat Settings
-    CompletionToIgnoreFate = 80         --If the fate has more than this much progress already, skip it
-    MinTimeLeftToIgnoreFate = 2*60      --If the fate has less than this many seconds left on the timer, skip it
-    CompletionToJoinBossFate = 5        --If the boss fate has less than this much progress, skip it (used to avoid soloing bosses)
-        CompletionToJoinSpecialBossFates = 20   --For the Special Fates like the Serpentlord Seethes or Mascot Murder
-        ClassForBossFates = ""              --If you want to use a different class for boss fates, set this to the 3 letter abbreviation
-                                        --for the class. Ex: "PLD"
-    JoinCollectionsFates = true         --Set to false if you never want to do collections fates
-    RSRAoeType = "Full"               --Options: Cleave/Full/Off
-    RSRAutoType = "HighHP"               --Options: LowHP/HighHP/Big/Small/HighMaxHP/LowMaxHP/Nearest/Farthest.
-                                    
-    UseBM = true                        --if you want to use the BossMod dodge/follow mode
-        BMorBMR = "BMR"
-        MeleeDist = 2.5                     --distance for BMRAI melee. Melee attacks (auto attacks) max distance is 2.59y, 2.60 is "target out of range"
-        RangedDist = 15                     --distance for BMRAI ranged. Ranged attacks and spells max distance to be usable is 25.49y, 25.5 is "target out of range"=
-
---Post Fate Settings
-    EnableChangeInstance = true                     --should it Change Instance when there is no Fate (only works on DT fates)
-        WaitIfBonusBuff = true                          --Don't change instances if you have the Twist of Fate bonus buff
-    ShouldExchangeBicolorVouchers = true            --Should it exchange Bicolor Gemstone Vouchers?
-        VoucherType = "Bicolor Gemstone Voucher"        -- Old Sharlayan for "Bicolor Gemstone Voucher" and Solution Nine for "Turali Bicolor Gemstone Voucher"
-    SelfRepair = false                              --if false, will go to Limsa mender
-        RepairAmount = 99                               --the amount it needs to drop before Repairing (set it to 0 if you don't want it to repair)
-        ShouldAutoBuyDarkMatter = true                  --Automatically buys a 99 stack of Grade 8 Dark Matter from the Limsa gil vendor if you're out
-    ShouldExtractMateria = true                           --should it Extract Materia
-    Retainers = false                                --should it do Retainers
-    ShouldGrandCompanyTurnIn = false                --should it to Turn ins at the GC (requires Deliveroo)
-        InventorySlotsLeft = 5  
+    yield("/echo Starting Xu configuration profile.")
+-- URL of the Lua script to fetch and execute
+    local scriptURL = "https://raw.githubusercontent.com/maherzord/pot0to-SND-Scripts/refs/heads/main/Fate%20Farming/Configuration%20Profile/Xu.lua"
+--local scriptURL = "https://example.com/script.lua"
+-- Execute the Lua script fetched from the URL
+    executeScriptFromURL(scriptURL)  
 end
 --Change this value for how much echos u want in chat 
 --0 no echos
