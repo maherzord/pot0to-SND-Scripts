0c57d9d41ebaacc7fe7deca510eadc84dc5b0380
maherzord <77556001+maherzord@users.noreply.github.com>
2024-10-24 16:21:11 -0300

    adding string config

--- Fate Farming/Fate Farming.lua
+++ Fate Farming/Fate Farming.lua
@@ -81,7 +81,8 @@ This Plugins are Optional and not needed unless you have it enabled in the setti
 player_first_string = string.sub(GetCharacterName(),1,1)
 
 --Pre Fate Settings
-if player_first_string = "S" then
+if player_first_string == "S" then
+    yield("/echo Starting "..player_first_string.." configuration.")
     Food = ""                           --Leave "" Blank if you don't want to use any food. If its HQ include <hq> next to the name "Baked Eggplant <hq>"
     Potion = ""                         --Leave "" Blank if you don't want to use any potions.
     ShouldSummonChocobo = true          --Summon chocobo?
@@ -116,7 +117,8 @@ if player_first_string = "S" then
     Retainers = false                                --should it do Retainers
     ShouldGrandCompanyTurnIn = false                --should it to Turn ins at the GC (requires Deliveroo)
         InventorySlotsLeft = 5                          --how much inventory space before turning in
-elseif player_first_string = "H" then
+elseif player_first_string == "H" then
+    yield("/echo Starting "..player_first_string.." configuration.")        
     Food = ""                           --Leave "" Blank if you don't want to use any food. If its HQ include <hq> next to the name "Baked Eggplant <hq>"
     Potion = ""                         --Leave "" Blank if you don't want to use any potions.
     ShouldSummonChocobo = true          --Summon chocobo?
@@ -137,7 +139,7 @@ elseif player_first_string = "H" then
     UseBM = true                        --if you want to use the BossMod dodge/follow mode
         BMorBMR = "BMR"
         MeleeDist = 2.5                     --distance for BMRAI melee. Melee attacks (auto attacks) max distance is 2.59y, 2.60 is "target out of range"
-        RangedDist = 10                     --distance for BMRAI ranged. Ranged attacks and spells max distance to be usable is 25.49y, 25.5 is "target out of range"=
+        RangedDist = 15                     --distance for BMRAI ranged. Ranged attacks and spells max distance to be usable is 25.49y, 25.5 is "target out of range"=
 
 --Post Fate Settings
     EnableChangeInstance = true                     --should it Change Instance when there is no Fate (only works on DT fates)
