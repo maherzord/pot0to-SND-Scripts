c5e75d2089febcae9c7df606be7cc564d775f0a9
maherzord <77556001+maherzord@users.noreply.github.com>
2024-11-22 13:51:23 -0300

    Update Fate Farming.lua

--- Fate Farming/Fate Farming.lua
+++ Fate Farming/Fate Farming.lua
@@ -70,50 +70,16 @@ This Plugins are Optional and not needed unless you have it enabled in the setti
 ]]
 
 --Pre Fate Settings
-Food = ""                           --Leave "" Blank if you don't want to use any food. If its HQ include <hq> next to the name "Baked Eggplant <hq>"
-Potion = ""                         --Leave "" Blank if you don't want to use any potions.
-ShouldSummonChocobo = true          --Summon chocobo?
-    ResummonChocoboTimeLeft = 3 * 60            --Resummons chocobo if there's less than this many seconds left on the timer, so it doesn't disappear on you in the middle of a fate.
-    ShouldAutoBuyGysahlGreens = true    --Automatically buys a 99 stack of Gysahl Greens from the Limsa gil vendor if you're out
-MountToUse = "mount roulette"       --The mount you'd like to use when flying between fates
-
---Fate Combat Settings
-CompletionToIgnoreFate = 80         --If the fate has more than this much progress already, skip it
-MinTimeLeftToIgnoreFate = 3*60      --If the fate has less than this many seconds left on the timer, skip it
-CompletionToJoinBossFate = 0        --If the boss fate has less than this much progress, skip it (used to avoid soloing bosses)
-    CompletionToJoinSpecialBossFates = 20   --For the Special Fates like the Serpentlord Seethes or Mascot Murder
-    ClassForBossFates = ""              --If you want to use a different class for boss fates, set this to the 3 letter abbreviation
-                                        --for the class. Ex: "PLD"
-JoinCollectionsFates = true         --Set to false if you never want to do collections fates
-RSRAoeType = "Full"               --Options: Cleave/Full/Off
-RSRAutoType = "HighHP"               --Options: LowHP/HighHP/Big/Small/HighMaxHP/LowMaxHP/Nearest/Farthest.
-
-RotationPlugin = "RSR"              --Options: RSR/BMR/VBM/Wrath/None
-    RotationSingleTargetPreset = ""     --For BMR/VBM only. Preset name for aoe mode.
-    RotationAoePreset = ""              --For BMR/VBM only. Prset name for single target mode (for forlorns).
-    MeleeDist = 2.5                     --Distance for melee. Melee attacks (auto attacks) max distance is 2.59y, 2.60 is "target out of range"
-    RangedDist = 20                     --Distance for ranged. Ranged attacks and spells max distance to be usable is 25.49y, 25.5 is "target out of range"=
-
-IgnoreForlorns = false
-    IgnoreBigForlornOnly = false
-
---Post Fate Settings
-EnableChangeInstance = true                     --should it Change Instance when there is no Fate (only works on DT fates)
-    WaitIfBonusBuff = true                          --Don't change instances if you have the Twist of Fate bonus buff
-ShouldExchangeBicolorVouchers = true            --Should it exchange Bicolor Gemstone Vouchers?
-    VoucherType = "Turali Bicolor Gemstone Voucher"        -- Old Sharlayan for "Bicolor Gemstone Voucher" and Solution Nine for "Turali Bicolor Gemstone Voucher"
-SelfRepair = false                              --if false, will go to Limsa mender
-    RepairAmount = 20                               --the amount it needs to drop before Repairing (set it to 0 if you don't want it to repair)
-    ShouldAutoBuyDarkMatter = true                  --Automatically buys a 99 stack of Grade 8 Dark Matter from the Limsa gil vendor if you're out
-ShouldExtractMateria = true                           --should it Extract Materia
-Retainers = true                                --should it do Retainers
-ShouldGrandCompanyTurnIn = false                --should it to Turn ins at the GC (requires Deliveroo)
-    InventorySlotsLeft = 5                          --how much inventory space before turning in
-
-Echo = "All"                                   --Options: All/Gems/None
-
-CompanionScriptMode = false                      --Set to true if you are using the fate script with a companion script (such as the Atma Farmer)
-
+if configuration_profile == "" then
+        yield("/echo No configuration profile detected, starting without profile input.")
+else
+        yield("/echo Starting "..configuration_profile.." configuration profile.")
+-- URL of the Lua script to fetch and execute
+        local scriptURL = "https://raw.githubusercontent.com/maherzord/pot0to-SND-Scripts/refs/heads/main/Fate%20Farming/Configuration%20Profile/"..configuration_profile..".lua"
+--local scriptURL = "https://example.com/script.lua"
+-- Execute the Lua script fetched from the URL
+        executeScriptFromURL(scriptURL) 
+end
 --#endregion Settings
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------
